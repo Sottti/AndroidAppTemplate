fac573f248ce72d3925c66c7d13e6ad82f9ac8c5
Pablo Costa Tirado <sottti@gmail.com>
2025-10-29 15:41:39 +0100

    Adds Padding.kt and Spacer.kt

---
+++ presentation/utils/src/main/kotlin/com/sottti/android/app/template/presentation/utils/Padding.kt
@@ -0,0 +1,24 @@
+package com.sottti.android.app.template.presentation.utils
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.platform.LocalLayoutDirection
+
+@Composable
+@ReadOnlyComposable
+public operator fun PaddingValues.plus(
+    extraPadding: PaddingValues,
+): PaddingValues {
+    val layoutDirection = LocalLayoutDirection.current
+    return PaddingValues(
+        top = calculateTopPadding() + extraPadding.calculateTopPadding(),
+        bottom = calculateBottomPadding() + extraPadding.calculateBottomPadding(),
+        start = calculateStartPadding(layoutDirection) +
+                extraPadding.calculateStartPadding(layoutDirection),
+        end = calculateEndPadding(layoutDirection) +
+                extraPadding.calculateEndPadding(layoutDirection),
+    )
+}
