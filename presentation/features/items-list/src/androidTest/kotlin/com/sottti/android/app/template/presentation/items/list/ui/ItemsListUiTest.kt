f27e83d012da2a73461a4d51dba385cf3a74c7a9
Pablo Costa Tirado <sottti@gmail.com>
2025-11-02 12:58:08 +0100

    - Having great fun with compose testing. - All tests passing.

--- presentation/features/items-list/src/androidTest/kotlin/com/sottti/android/app/template/presentation/items/list/ui/ItemsListUiTest.kt
+++ presentation/features/items-list/src/androidTest/kotlin/com/sottti/android/app/template/presentation/items/list/ui/ItemsListUiTest.kt
@@ -1,5 +1,6 @@
 package com.sottti.android.app.template.presentation.items.list.ui
 
+import androidx.compose.ui.test.SemanticsMatcher
 import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
@@ -11,301 +12,156 @@ import com.sottti.android.app.template.presentation.design.system.error.ERROR_TE
 import com.sottti.android.app.template.presentation.design.system.progress.indicators.PROGRESS_INDICATOR_TEST_TAG
 import com.sottti.android.app.template.presentation.utils.test.BaseUiTest
 import com.sottti.android.app.template.presentation.utils.test.OrientationTest
-import com.sottti.android.app.template.presentation.utils.test.setLandscape
-import com.sottti.android.app.template.presentation.utils.test.setPortrait
+import com.sottti.android.app.template.presentation.utils.test.setOrientation
 import org.junit.Test
 
 internal class ItemsListUiTest(
     orientation: OrientationTest,
 ) : BaseUiTest(orientation) {
 
+    private fun fullscreenIndicatorMatcher(): SemanticsMatcher =
+        hasTestTag(PROGRESS_INDICATOR_TEST_TAG) and
+                hasTestTag(PROGRESS_INDICATOR_GRID_TEST_TAG).not()
+
+    private fun gridIndicatorMatcher(): SemanticsMatcher =
+        hasTestTag(PROGRESS_INDICATOR_GRID_TEST_TAG)
+
     @Test
     fun givenStateIsInitialLoad_whenUiIsRendered_thenFullScreenProgressBarsAreVisible() {
-        when (orientation) {
-            OrientationTest.Portrait -> rule.setPortrait()
-            OrientationTest.Landscape -> rule.setLandscape()
-        }
+        rule.setOrientation(orientation)
 
         rule.setContent { ItemListUiPreview(state = loadingState) }
 
-        rule
-            .onNodeWithTag(PROGRESS_INDICATOR_TEST_TAG)
-            .assertIsDisplayed()
+        rule.onNode(fullscreenIndicatorMatcher()).assertIsDisplayed()
+        rule.onNode(gridIndicatorMatcher()).assertDoesNotExist()
 
-        rule
-            .onNodeWithTag(PULL_TO_REFRESH_TEST_TAG)
-            .assertIsDisplayed()
+        rule.onNodeWithTag(PULL_TO_REFRESH_TEST_TAG).assertIsDisplayed()
     }
 
     @Test
     fun givenStateIsAppendLoading_whenUiIsRendered_thenAppendIndicatorAndItemsAreVisible() {
-        when (orientation) {
-            OrientationTest.Portrait -> rule.setPortrait()
-            OrientationTest.Landscape -> rule.setLandscape()
-        }
+        rule.setOrientation(orientation)
 
         rule.setContent { ItemListUiPreview(state = loadedStateAppendLoading) }
 
-        rule.onNode(
-            hasTestTag(PROGRESS_INDICATOR_GRID_TEST_TAG).not() and
-                    hasTestTag(PROGRESS_INDICATOR_TEST_TAG)
-        )
-            .assertIsNotDisplayed()
+        rule.onNode(fullscreenIndicatorMatcher()).assertDoesNotExist()
+        rule.onNode(gridIndicatorMatcher()).assertIsDisplayed()
 
-        rule
-            .onNodeWithTag(PROGRESS_INDICATOR_GRID_TEST_TAG)
-            .assertIsDisplayed()
-
-        rule
-            .onAllNodesWithTag(GRID_ITEM_TEST_TAG)
-            .assertCountEquals(2)
+        rule.onAllNodesWithTag(GRID_ITEM_TEST_TAG).assertCountEquals(2)
     }
 
     @Test
     fun givenStateIsPrependLoading_whenUiIsRendered_thenPrependIndicatorAndItemsAreVisible() {
-        when (orientation) {
-            OrientationTest.Portrait -> rule.setPortrait()
-            OrientationTest.Landscape -> rule.setLandscape()
-        }
+        rule.setOrientation(orientation)
 
         rule.setContent { ItemListUiPreview(state = loadedStatePrependLoading) }
 
-        rule.onNode(
-            hasTestTag(PROGRESS_INDICATOR_GRID_TEST_TAG).not() and
-                    hasTestTag(PROGRESS_INDICATOR_TEST_TAG)
-        )
-            .assertIsNotDisplayed()
+        rule.onNode(fullscreenIndicatorMatcher()).assertDoesNotExist()
+        rule.onNode(gridIndicatorMatcher()).assertIsDisplayed()
 
-        rule
-            .onNodeWithTag(PROGRESS_INDICATOR_GRID_TEST_TAG)
-            .assertIsDisplayed()
-
-        rule
-            .onAllNodesWithTag(GRID_ITEM_TEST_TAG)
-            .assertCountEquals(2)
+        rule.onAllNodesWithTag(GRID_ITEM_TEST_TAG).assertCountEquals(2)
     }
 
     @Test
     fun givenStateIsAppendPrependLoading_whenUiIsRendered_thenAppendAndPrependIndicatorsAndItemsAreVisible() {
-        when (orientation) {
-            OrientationTest.Portrait -> rule.setPortrait()
-            OrientationTest.Landscape -> rule.setLandscape()
-        }
+        rule.setOrientation(orientation)
 
         rule.setContent { ItemListUiPreview(state = loadedStateAppendPrependBothLoading) }
 
-        rule.onNode(
-            hasTestTag(PROGRESS_INDICATOR_GRID_TEST_TAG).not() and
-                    hasTestTag(PROGRESS_INDICATOR_TEST_TAG)
-        )
-            .assertIsNotDisplayed()
-
-        rule
-            .onAllNodesWithTag(PROGRESS_INDICATOR_GRID_TEST_TAG)
-            .assertCountEquals(2)
-
-        rule
-            .onAllNodesWithTag(GRID_ITEM_TEST_TAG)
-            .assertCountEquals(2)
+        rule.onNode(fullscreenIndicatorMatcher()).assertDoesNotExist()
+        rule.onAllNodes(gridIndicatorMatcher()).assertCountEquals(2)
+        rule.onAllNodesWithTag(GRID_ITEM_TEST_TAG).assertCountEquals(2)
     }
 
     @Test
     fun givenStateIsLoadedNoPagination_whenUiIsRendered_thenItemsAreVisible() {
-        when (orientation) {
-            OrientationTest.Portrait -> rule.setPortrait()
-            OrientationTest.Landscape -> rule.setLandscape()
-        }
+        rule.setOrientation(orientation)
 
         rule.setContent { ItemListUiPreview(state = loadedStateNoPagination) }
 
-        rule.onNode(
-            hasTestTag(PROGRESS_INDICATOR_GRID_TEST_TAG).not() and
-                    hasTestTag(PROGRESS_INDICATOR_TEST_TAG)
-        )
-            .assertIsNotDisplayed()
-
-        rule
-            .onNodeWithTag(PROGRESS_INDICATOR_GRID_TEST_TAG)
-            .assertIsNotDisplayed()
-
-        rule
-            .onAllNodesWithTag(GRID_ITEM_TEST_TAG)
-            .assertCountEquals(2)
+        rule.onNode(fullscreenIndicatorMatcher()).assertDoesNotExist()
+        rule.onNode(gridIndicatorMatcher()).assertDoesNotExist()
+        rule.onAllNodesWithTag(GRID_ITEM_TEST_TAG).assertCountEquals(2)
     }
 
     @Test
     fun givenStateIsLoadedStateAppendEndReached_whenUiIsRendered_thenItemsAreVisible() {
-        when (orientation) {
-            OrientationTest.Portrait -> rule.setPortrait()
-            OrientationTest.Landscape -> rule.setLandscape()
-        }
+        rule.setOrientation(orientation)
 
         rule.setContent { ItemListUiPreview(state = loadedStateAppendEndReached) }
 
-        rule.onNode(
-            hasTestTag(PROGRESS_INDICATOR_GRID_TEST_TAG).not() and
-                    hasTestTag(PROGRESS_INDICATOR_TEST_TAG)
-        )
-            .assertIsNotDisplayed()
-
-        rule
-            .onNodeWithTag(PROGRESS_INDICATOR_GRID_TEST_TAG)
-            .assertIsNotDisplayed()
-
-        rule
-            .onAllNodesWithTag(GRID_ITEM_TEST_TAG)
-            .assertCountEquals(2)
+        rule.onNode(fullscreenIndicatorMatcher()).assertDoesNotExist()
+        rule.onNode(gridIndicatorMatcher()).assertDoesNotExist()
+        rule.onAllNodesWithTag(GRID_ITEM_TEST_TAG).assertCountEquals(2)
     }
 
     @Test
     fun givenStateIsLoadedStatePrependEndReached_whenUiIsRendered_thenItemsAreVisible() {
-        when (orientation) {
-            OrientationTest.Portrait -> rule.setPortrait()
-            OrientationTest.Landscape -> rule.setLandscape()
-        }
+        rule.setOrientation(orientation)
 
         rule.setContent { ItemListUiPreview(state = loadedStatePrependEndReached) }
 
-        rule.onNode(
-            hasTestTag(PROGRESS_INDICATOR_GRID_TEST_TAG).not() and
-                    hasTestTag(PROGRESS_INDICATOR_TEST_TAG)
-        )
-            .assertIsNotDisplayed()
-
-        rule
-            .onNodeWithTag(PROGRESS_INDICATOR_GRID_TEST_TAG)
-            .assertIsNotDisplayed()
-
-        rule
-            .onAllNodesWithTag(GRID_ITEM_TEST_TAG)
-            .assertCountEquals(2)
+        rule.onNode(fullscreenIndicatorMatcher()).assertDoesNotExist()
+        rule.onNode(gridIndicatorMatcher()).assertDoesNotExist()
+        rule.onAllNodesWithTag(GRID_ITEM_TEST_TAG).assertCountEquals(2)
     }
 
     @Test
     fun givenStateIsLoadedStateAppendPrependBothEndsReached_whenUiIsRendered_thenItemsAreVisible() {
-        when (orientation) {
-            OrientationTest.Portrait -> rule.setPortrait()
-            OrientationTest.Landscape -> rule.setLandscape()
-        }
+        rule.setOrientation(orientation)
 
         rule.setContent { ItemListUiPreview(state = loadedStateAppendPrependBothEndsReached) }
 
-        rule.onNode(
-            hasTestTag(PROGRESS_INDICATOR_GRID_TEST_TAG).not() and
-                    hasTestTag(PROGRESS_INDICATOR_TEST_TAG)
-        )
-            .assertIsNotDisplayed()
-
-        rule
-            .onNodeWithTag(PROGRESS_INDICATOR_GRID_TEST_TAG)
-            .assertIsNotDisplayed()
-
-        rule
-            .onAllNodesWithTag(GRID_ITEM_TEST_TAG)
-            .assertCountEquals(2)
+        rule.onNode(fullscreenIndicatorMatcher()).assertDoesNotExist()
+        rule.onNode(gridIndicatorMatcher()).assertDoesNotExist()
+        rule.onAllNodesWithTag(GRID_ITEM_TEST_TAG).assertCountEquals(2)
     }
 
     @Test
     fun givenStateIsLoadedStateLoadsOfItems_whenUiIsRendered_thenItemsAreVisible() {
-        when (orientation) {
-            OrientationTest.Portrait -> rule.setPortrait()
-            OrientationTest.Landscape -> rule.setLandscape()
-        }
+        rule.setOrientation(orientation)
 
         rule.setContent { ItemListUiPreview(state = loadedStateLoadsOfItems) }
 
-        rule.onNode(
-            hasTestTag(PROGRESS_INDICATOR_GRID_TEST_TAG).not() and
-                    hasTestTag(PROGRESS_INDICATOR_TEST_TAG)
-        ).assertIsNotDisplayed()
-
-        rule
-            .onNodeWithTag(PROGRESS_INDICATOR_GRID_TEST_TAG)
-            .assertIsNotDisplayed()
-
-        rule
-            .onAllNodesWithTag(GRID_ITEM_TEST_TAG)
-            .assertCountEquals(10)
+        rule.onNode(fullscreenIndicatorMatcher()).assertDoesNotExist()
+        rule.onNode(gridIndicatorMatcher()).assertDoesNotExist()
+        rule.onAllNodesWithTag(GRID_ITEM_TEST_TAG).assertCountEquals(10)
     }
 
     @Test
     fun givenStateIsLoadedStateLoadsOfItemsRefreshing_whenUiIsRendered_thenItemsAreVisible() {
-        when (orientation) {
-            OrientationTest.Portrait -> rule.setPortrait()
-            OrientationTest.Landscape -> rule.setLandscape()
-        }
+        rule.setOrientation(orientation)
 
         rule.setContent { ItemListUiPreview(state = loadedStateLoadsOfItemsRefreshing) }
 
-        rule.onNode(
-            hasTestTag(PROGRESS_INDICATOR_GRID_TEST_TAG).not() and
-                    hasTestTag(PROGRESS_INDICATOR_TEST_TAG)
-        ).assertIsNotDisplayed()
+        rule.onNode(fullscreenIndicatorMatcher()).assertDoesNotExist()
+        rule.onNode(gridIndicatorMatcher()).assertIsNotDisplayed()
 
-        rule
-            .onNodeWithTag(PROGRESS_INDICATOR_GRID_TEST_TAG)
-            .assertIsNotDisplayed()
-
-        rule
-            .onNodeWithTag(PULL_TO_REFRESH_TEST_TAG)
-            .assertIsDisplayed()
-
-        rule
-            .onAllNodesWithTag(GRID_ITEM_TEST_TAG)
-            .assertCountEquals(10)
+        rule.onNodeWithTag(PULL_TO_REFRESH_TEST_TAG).assertIsDisplayed()
+        rule.onAllNodesWithTag(GRID_ITEM_TEST_TAG).assertCountEquals(10)
     }
 
     @Test
-    fun givenStateIsErrorState_whenUiIsRendered_thenEmptyIsVisible() {
-        when (orientation) {
-            OrientationTest.Portrait -> rule.setPortrait()
-            OrientationTest.Landscape -> rule.setLandscape()
-        }
+    fun givenStateIsEmptyState_whenUiIsRendered_thenEmptyIsVisible() {
+        rule.setOrientation(orientation)
 
         rule.setContent { ItemListUiPreview(state = emptyState) }
 
-        rule.onNode(
-            hasTestTag(PROGRESS_INDICATOR_GRID_TEST_TAG).not() and
-                    hasTestTag(PROGRESS_INDICATOR_TEST_TAG)
-        ).assertIsNotDisplayed()
-
-        rule
-            .onNodeWithTag(PROGRESS_INDICATOR_GRID_TEST_TAG)
-            .assertIsNotDisplayed()
-
-        rule
-            .onAllNodesWithTag(GRID_ITEM_TEST_TAG)
-            .assertCountEquals(0)
-
-        rule
-            .onNodeWithTag(ERROR_TEST_TAG)
-            .assertIsDisplayed()
+        rule.onNode(fullscreenIndicatorMatcher()).assertDoesNotExist()
+        rule.onNode(gridIndicatorMatcher()).assertDoesNotExist()
+        rule.onAllNodesWithTag(GRID_ITEM_TEST_TAG).assertCountEquals(0)
+        rule.onNodeWithTag(EMPTY_TEST_TAG).assertIsDisplayed()
     }
 
     @Test
     fun givenStateIsErrorState_whenUiIsRendered_thenErrorIsVisible() {
-        when (orientation) {
-            OrientationTest.Portrait -> rule.setPortrait()
-            OrientationTest.Landscape -> rule.setLandscape()
-        }
+        rule.setOrientation(orientation)
 
         rule.setContent { ItemListUiPreview(state = errorState) }
 
-        rule.onNode(
-            hasTestTag(PROGRESS_INDICATOR_GRID_TEST_TAG).not() and
-                    hasTestTag(PROGRESS_INDICATOR_TEST_TAG)
-        ).assertIsNotDisplayed()
-
-        rule
-            .onNodeWithTag(PROGRESS_INDICATOR_GRID_TEST_TAG)
-            .assertIsNotDisplayed()
-
-        rule
-            .onAllNodesWithTag(GRID_ITEM_TEST_TAG)
-            .assertCountEquals(0)
-
-        rule
-            .onNodeWithTag(EMPTY_TEST_TAG)
-            .assertIsDisplayed()
+        rule.onNode(fullscreenIndicatorMatcher()).assertDoesNotExist()
+        rule.onNode(gridIndicatorMatcher()).assertDoesNotExist()
+        rule.onAllNodesWithTag(GRID_ITEM_TEST_TAG).assertCountEquals(0)
+        rule.onNodeWithTag(ERROR_TEST_TAG).assertIsDisplayed()
     }
 }
--- presentation/utils/src/main/kotlin/com/sottti/android/app/template/presentation/utils/test/SetOrientationTest.kt
+++ presentation/utils/src/main/kotlin/com/sottti/android/app/template/presentation/utils/test/SetOrientationTest.kt
@@ -5,12 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 
-public fun <A : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<A>, A>.setPortrait() {
+public fun <A : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<A>, A>.setOrientation(
+    orientation: OrientationTest,
+) {
+    when (orientation) {
+        OrientationTest.Portrait -> setPortrait()
+        OrientationTest.Landscape -> setLandscape()
+    }
+}
+
+internal fun <A : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<A>, A>.setPortrait() {
     activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
     waitForIdle()
 }
 
-public fun <A : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<A>, A>.setLandscape() {
+internal fun <A : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<A>, A>.setLandscape() {
     activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
     waitForIdle()
 }
