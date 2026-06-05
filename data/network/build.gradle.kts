e4553dab814ae08fa63a4c605290640ce7d98d6a
Pablo Costa Tirado <sottti@gmail.com>
2026-06-05 17:13:21 +0200

    Move network fakes to test fixtures

--- data/network/build.gradle.kts
+++ data/network/build.gradle.kts
@@ -8,25 +8,30 @@ plugins {
 
 android {
     namespace = "com.sottti.android.app.template.data.network"
+
+    testFixtures {
+        enable = true
+    }
 }
 
 dependencies {
     api(libs.ktor.client.core)
     api(libs.result)
     implementation(libs.coil.network.ktor3)
-    implementation(libs.coil.test)
     implementation(libs.hilt)
-    implementation(libs.hilt.android.testing)
     implementation(libs.ktor.client.cio)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.client.logging)
-    implementation(libs.ktor.client.mock)
     implementation(libs.ktor.serialization.kotlinx.json)
-    implementation(project(module.presentation.designSystem.images))
     ksp(libs.hilt.compiler)
 
     testImplementation(libs.junit)
     testImplementation(libs.kotlin.coroutines.test)
     testImplementation(libs.ktor.client.mock)
     testImplementation(libs.truth)
+    testFixturesImplementation(libs.coil.test)
+    testFixturesImplementation(libs.hilt.android.testing)
+    testFixturesImplementation(libs.kotlinx.serialization.json)
+    testFixturesImplementation(libs.ktor.client.mock)
+    testFixturesImplementation(project(module.presentation.designSystem.images))
 }
